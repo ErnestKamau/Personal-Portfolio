3c957329477f23de1de983fc7e5c5fad3deea6f4
Ernest Kamau <1.kamauernest@gmail.com>
2025-08-14 16:05:57 +0300

    Scroll into view to navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 function Navbar() {
+    const handleScroll = (sectionId) => {
+        const section = document.getElementById(sectionId)
+        if (section) {
+            section.scrollIntoView({behavior: "smooth"})
+        }
+    }
 
     return(
         <nav className="fixed top-0 left-0 w-full bg-white shadow p-4 z-50 flex gap-6">
-            <button> Home </button>
-            <button> About-me </button>
-            <button> projects </button>
-            <button> services</button>
+            <button onClick={()=>handleScroll('home')}> Home </button>
+            <button onClick={()=>handleScroll('')}> About-me </button>
+            <button onClick={()=>handleScroll('skills')}> projects </button>
+            <button onClick={()=>handleScroll('projects')}> projects </button>
         </nav>
     )
 
