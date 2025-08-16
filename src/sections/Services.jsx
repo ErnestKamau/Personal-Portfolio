a4db3ff4ff265cdf00ca3682d4f82a28012a2828
Ernest Kamau <1.kamauernest@gmail.com>
2025-08-16 22:07:48 +0300

    Finish services offerred section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,4 +1,9 @@
 import { AppWindow } from "lucide-react"
+import { FaServer } from "react-icons/fa"
+import { CgWebsite } from "react-icons/cg"
+import { MdOutlineWeb } from "react-icons/md"
+import { RiLayoutMasonryLine } from "react-icons/ri"
+
 
 function Services(){
 
@@ -9,14 +14,14 @@ function Services(){
                     <h2 className="text-3xl font-bold text-center text-gray-900 mb-9 ">Services I offer to my clients</h2>
                     <div className="grid grid-col-1 md:grid-cols-3 gap-8">
                         
-                        <div className="shadow-md hover:shadow-lg transition-shadow p-6 w-60">
+                        <div className="shadow-md hover:shadow-lg hover:bg-blue-50 transition-all p-6 w-60">
                             <div className="mb-4">
-                                <AppWindow/>
+                                <CgWebsite size={24}/>
                             </div>
 
                             <h3 className="text-2xl font-bold text-gray-700 text-wrap mb-4">Web Development</h3>
                             <div className="flex flex-wrap gap-2 mb-2">
-                                <span>I create responsive and modern websites tailored to your needs.</span>
+                                <span>I create responsive and modern websites tailored to your needs. </span>
                             </div>
                             <div className="px-3 text-sm ">
                                 <ul className="list-disc list-inside">
@@ -29,7 +34,45 @@ function Services(){
                             
                         </div>
 
-                        
+                        <div className="shadow-md hover:shadow-lg hover:bg-blue-50 transition-all p-6 w-60">
+                            <div className="mb-4">
+                                <FaServer/>
+                            </div>
+
+                            <h3 className="text-2xl font-bold text-gray-700 text-wrap mb-4">Backend Development</h3>
+                            <div className="flex flex-wrap gap-2 mb-2">
+                                <span>I build robust backend systems to support your web applications.</span>
+                            </div>
+                            <div className="px-3 text-sm ">
+                                <ul className="list-disc list-inside">
+                                    <li>Python, Django and Flask</li>
+                                    <li>API Development</li>
+                                    <li>Server Management</li>
+                                    <li>Database Management</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+
+                        <div className="shadow-md hover:shadow-lg hover:bg-blue-50 transition-all p-6 w-60">
+                            <div className="mb-4">
+                                <RiLayoutMasonryLine size={24}/>
+                            </div>
+
+                            <h3 className="text-2xl font-bold text-gray-700 text-wrap mb-4">Frontend Development</h3>
+                            <div className="flex flex-wrap gap-2 mb-2">
+                                <span>I create user-friendly interfaces that bring your ideas to life. </span>
+                            </div>
+                            <div className="px-3 text-sm ">
+                                <ul className="list-disc list-inside">
+                                    <li>Custom website design</li>
+                                    <li>Interactive UI</li>
+                                    <li>Modern styling.</li>
+                                    <li>API integration for dynamic content</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
 
 
 
